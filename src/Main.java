b46af2ad59da7221ffecacb3b25d3c1c9d49ee7d
steph <stephenbenny050@gmail.com>
2024-09-19 13:30:04 -0400

    test

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int intOperandA = 32;
+        int intOperandB = 14;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double doubleOperandA = 23.23;
+        double doubleOperandB = 3.14;
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleModulo = 0;
+
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intSum );
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intProduct );
+
+
+
+
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+        intDifference = intOperandA - intOperandB;
+
+
+    }
+}
